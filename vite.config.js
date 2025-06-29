3d575c8fc7767ceb159be122118be35c58c92484
Manohar Dungala <manohardungala@gmail.com>
2025-06-29 15:15:37 +0530

    Merge

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Contact-Us-Page/', // ✅ important for GitHub Pages
   plugins: [react()],
 })
